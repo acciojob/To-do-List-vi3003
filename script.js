776b2d59b747aa3d4b02f44bd9d54cdd13446304
vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-02-14 21:16:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-//your code here
+const inp = document.getElementById('newTodoInput');
+const btn = document.getElementById('addTodoBtn');
+const arr = [];
+const listBody = document.getElementById('todoList');
+
+// add item to list on click function
+function addItem() {
+    console.log('hello');
+    let inpItem = inp.value;
+    if (inpItem === '') {
+        alert('Add item first');
+        return;
+    }
+    arr.push(inpItem);
+    console.log(arr);
+    listBody.innerHTML = '';
+    arr.map((item, index) => {
+        listBody.innerHTML += `<li>${item}</li>`
+    })
+    inp.value = '';
+}
 
